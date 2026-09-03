3baf1aad6dcefb1d77f684216f7724c8b32081ea
dduxx <nmagee@unb.ca>
2026-09-02 23:38:38 -0300

    feat: added goblin support

---
+++ scripts/goblin/goblin_adult.sh
@@ -0,0 +1,47 @@
+# Right shoulder: (3,2) and (3,1)
+2point5dinfo -sa -p 1 -s 0.2 -l "${DWARF_FORTRESS_INSTALL_DIR}"/data/vanilla/vanilla_creatures_graphics/graphics/images/goblin/goblin_body.png:96,64,128,96 -o "${FIXTURE_OUTPUT_DIR}"/goblin/adult_right_shoulder_f.json
+2point5dinfo -sa -p 1 -s 0.2 -l "${DWARF_FORTRESS_INSTALL_DIR}"/data/vanilla/vanilla_creatures_graphics/graphics/images/goblin/goblin_body.png:96,32,128,64 -o "${FIXTURE_OUTPUT_DIR}"/goblin/adult_right_shoulder_m.json
+
+# Right hand: (2,2) and (2,1)
+2point5dinfo -sa -p 1 -s 0.2 -l "${DWARF_FORTRESS_INSTALL_DIR}"/data/vanilla/vanilla_creatures_graphics/graphics/images/goblin/goblin_body.png:64,64,96,96 -o "${FIXTURE_OUTPUT_DIR}"/goblin/adult_right_hand_f.json
+2point5dinfo -sa -p 1 -s 0.2 -l "${DWARF_FORTRESS_INSTALL_DIR}"/data/vanilla/vanilla_creatures_graphics/graphics/images/goblin/goblin_body.png:64,32,96,64 -o "${FIXTURE_OUTPUT_DIR}"/goblin/adult_right_hand_m.json
+
+# Right leg: (1,2) and (1,1)
+2point5dinfo -sa -p 1 -s 0.2 -l "${DWARF_FORTRESS_INSTALL_DIR}"/data/vanilla/vanilla_creatures_graphics/graphics/images/goblin/goblin_body.png:32,64,64,96 -o "${FIXTURE_OUTPUT_DIR}"/goblin/adult_right_leg_f.json
+2point5dinfo -sa -p 1 -s 0.2 -l "${DWARF_FORTRESS_INSTALL_DIR}"/data/vanilla/vanilla_creatures_graphics/graphics/images/goblin/goblin_body.png:32,32,64,64 -o "${FIXTURE_OUTPUT_DIR}"/goblin/adult_right_leg_m.json
+
+# Right foot: (0,2) and (0,1)
+2point5dinfo -sa -p 1 -s 0.2 -l "${DWARF_FORTRESS_INSTALL_DIR}"/data/vanilla/vanilla_creatures_graphics/graphics/images/goblin/goblin_body.png:0,64,32,96 -o "${FIXTURE_OUTPUT_DIR}"/goblin/adult_right_foot_f.json
+2point5dinfo -sa -p 1 -s 0.2 -l "${DWARF_FORTRESS_INSTALL_DIR}"/data/vanilla/vanilla_creatures_graphics/graphics/images/goblin/goblin_body.png:0,32,32,64 -o "${FIXTURE_OUTPUT_DIR}"/goblin/adult_right_foot_m.json
+
+# Left leg: (7,2) and (7,1)
+2point5dinfo -sa -p 1 -s 0.2 -l "${DWARF_FORTRESS_INSTALL_DIR}"/data/vanilla/vanilla_creatures_graphics/graphics/images/goblin/goblin_body.png:224,64,256,96 -o "${FIXTURE_OUTPUT_DIR}"/goblin/adult_left_leg_f.json
+2point5dinfo -sa -p 1 -s 0.2 -l "${DWARF_FORTRESS_INSTALL_DIR}"/data/vanilla/vanilla_creatures_graphics/graphics/images/goblin/goblin_body.png:224,32,256,64 -o "${FIXTURE_OUTPUT_DIR}"/goblin/adult_left_leg_m.json
+
+# Left foot: (8,2) and (8,1)
+2point5dinfo -sa -p 1 -s 0.2 -l "${DWARF_FORTRESS_INSTALL_DIR}"/data/vanilla/vanilla_creatures_graphics/graphics/images/goblin/goblin_body.png:256,64,288,96 -o "${FIXTURE_OUTPUT_DIR}"/goblin/adult_left_foot_f.json
+2point5dinfo -sa -p 1 -s 0.2 -l "${DWARF_FORTRESS_INSTALL_DIR}"/data/vanilla/vanilla_creatures_graphics/graphics/images/goblin/goblin_body.png:256,32,288,64 -o "${FIXTURE_OUTPUT_DIR}"/goblin/adult_left_foot_m.json
+
+# Body: (4,2) and (4,1)
+2point5dinfo -sa -p 1 -s 0.2 -l "${DWARF_FORTRESS_INSTALL_DIR}"/data/vanilla/vanilla_creatures_graphics/graphics/images/goblin/goblin_body.png:128,64,160,96 -o "${FIXTURE_OUTPUT_DIR}"/goblin/adult_body_f.json
+2point5dinfo -sa -p 1 -s 0.2 -l "${DWARF_FORTRESS_INSTALL_DIR}"/data/vanilla/vanilla_creatures_graphics/graphics/images/goblin/goblin_body.png:128,32,160,64 -o "${FIXTURE_OUTPUT_DIR}"/goblin/adult_body_m.json
+
+# Left shoulder: (5,2) and (5,1)
+2point5dinfo -sa -p 1 -s 0.2 -l "${DWARF_FORTRESS_INSTALL_DIR}"/data/vanilla/vanilla_creatures_graphics/graphics/images/goblin/goblin_body.png:160,64,192,96 -o "${FIXTURE_OUTPUT_DIR}"/goblin/adult_left_shoulder_f.json
+2point5dinfo -sa -p 1 -s 0.2 -l "${DWARF_FORTRESS_INSTALL_DIR}"/data/vanilla/vanilla_creatures_graphics/graphics/images/goblin/goblin_body.png:160,32,192,64 -o "${FIXTURE_OUTPUT_DIR}"/goblin/adult_left_shoulder_m.json
+
+# Face female: (0,0) through (3,0)
+2point5dinfo -sa -p 1 -s 0.2 -l "${DWARF_FORTRESS_INSTALL_DIR}"/data/vanilla/vanilla_creatures_graphics/graphics/images/goblin/goblin_body.png:0,0,32,32 -o "${FIXTURE_OUTPUT_DIR}"/goblin/adult_face_f_1.json
+2point5dinfo -sa -p 1 -s 0.2 -l "${DWARF_FORTRESS_INSTALL_DIR}"/data/vanilla/vanilla_creatures_graphics/graphics/images/goblin/goblin_body.png:32,0,64,32 -o "${FIXTURE_OUTPUT_DIR}"/goblin/adult_face_f_2.json
+2point5dinfo -sa -p 1 -s 0.2 -l "${DWARF_FORTRESS_INSTALL_DIR}"/data/vanilla/vanilla_creatures_graphics/graphics/images/goblin/goblin_body.png:64,0,96,32 -o "${FIXTURE_OUTPUT_DIR}"/goblin/adult_face_f_3.json
+2point5dinfo -sa -p 1 -s 0.2 -l "${DWARF_FORTRESS_INSTALL_DIR}"/data/vanilla/vanilla_creatures_graphics/graphics/images/goblin/goblin_body.png:96,0,128,32 -o "${FIXTURE_OUTPUT_DIR}"/goblin/adult_face_f_4.json
+
+# Face male: (4,0) through (7,0)
+2point5dinfo -sa -p 1 -s 0.2 -l "${DWARF_FORTRESS_INSTALL_DIR}"/data/vanilla/vanilla_creatures_graphics/graphics/images/goblin/goblin_body.png:128,0,160,32 -o "${FIXTURE_OUTPUT_DIR}"/goblin/adult_face_m_1.json
+2point5dinfo -sa -p 1 -s 0.2 -l "${DWARF_FORTRESS_INSTALL_DIR}"/data/vanilla/vanilla_creatures_graphics/graphics/images/goblin/goblin_body.png:160,0,192,32 -o "${FIXTURE_OUTPUT_DIR}"/goblin/adult_face_m_2.json
+2point5dinfo -sa -p 1 -s 0.2 -l "${DWARF_FORTRESS_INSTALL_DIR}"/data/vanilla/vanilla_creatures_graphics/graphics/images/goblin/goblin_body.png:192,0,224,32 -o "${FIXTURE_OUTPUT_DIR}"/goblin/adult_face_m_3.json
+2point5dinfo -sa -p 1 -s 0.2 -l "${DWARF_FORTRESS_INSTALL_DIR}"/data/vanilla/vanilla_creatures_graphics/graphics/images/goblin/goblin_body.png:224,0,256,32 -o "${FIXTURE_OUTPUT_DIR}"/goblin/adult_face_m_4.json
+
+# Left hand: (6,2) and (6,1)
+2point5dinfo -sa -p 1 -s 0.2 -l "${DWARF_FORTRESS_INSTALL_DIR}"/data/vanilla/vanilla_creatures_graphics/graphics/images/goblin/goblin_body.png:192,64,224,96 -o "${FIXTURE_OUTPUT_DIR}"/goblin/adult_left_hand_f.json
+2point5dinfo -sa -p 1 -s 0.2 -l "${DWARF_FORTRESS_INSTALL_DIR}"/data/vanilla/vanilla_creatures_graphics/graphics/images/goblin/goblin_body.png:192,32,224,64 -o "${FIXTURE_OUTPUT_DIR}"/goblin/adult_left_hand_m.json
